8f251bc7ec7aab0ae73123003b7b02c7beb39423
Tram <91838259+Tram-ng@users.noreply.github.com>
2022-03-30 20:27:02 +0000

    the order of If-Else

--- main.py
+++ main.py
@@ -27,16 +27,27 @@ scissors = '''
 
 #Write your code below this line 👇
 import random
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
-computer_choice =  random.randint (0,2)
-print (f"Computer chose {computer_choice}")
-if computer_choice == 0 and user_choice == 2:
-  print ("Computer win!")
-elif user_choice == 0 and computer_choice == 2:
-  print ("User win!")
-elif user_choice > computer_choice:
-  print("User win!")
-elif computer_choice > user_choice:
-  print ("Computer win!")
-else : 
-  print ("Tie")
+
+game_images = [rock, paper, scissors]
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors "))
+if user_input >= 3 or user_input < 0:
+  print("Input invalid, please try again.")
+else:
+  print (game_images[user_input])
+  computer_choice = random.randint(0,2)
+  print("Computer chose {computer_choice}")
+  print(game_images[computer_choice])
+  if user_input ==  0 and computer_choice == 2:
+    print ("User win!")
+  elif user_input == 2 and computer_choice == 0:
+    print ("Computer win")
+  elif user_input >  computer_choice:
+    print  ("User win!")
+  elif user_input == computer_choice:
+    print("Tie")
+  else:
+    print ("Computer win")
+  
+
+
+  
